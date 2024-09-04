825947881d6e2f3e125461555544e6a8e6ca0384
mustafa-ali-1 <mustafa_ali_1@outlook.com>
2024-09-04 16:02:28 +0530

    Terraform-01'1

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-resource "local_file" "my_tree" {
-    filename = "plant.txt"
-    content = "Save Water for Future./"
-    lifecycle {
-      #ignore_changes
-    }
+resource "local_file" "my_trees" {
+    filename = "plants.txt"
+    content = "I'll preserve the nature!"
 }
 
+resource "random_pet" "my-trees" {
+    prefix = "unique/any_name"
+    separator = "."
+    length = "2"
+}
+
+# resource "local_file" "object-name-01-TF" {
+#     filename = "indoor-plants-${random_pet.name.id}.txt"
+#     content = "Herbivorous are dependent on plants"
+# }
+
+# resource "random_pet" "object-name-02-TF" {
+#     prefix = "unq-id"
+#     separator = "-"
+#     length = "1"
+# }
