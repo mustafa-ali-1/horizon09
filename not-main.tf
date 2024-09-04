8e039f1d484fe8467e4f8ec2e4c5ca8f6a2e493f
mustafa-ali-1 <mustafa_ali_1@outlook.com>
2024-09-04 18:15:05 +0530

    Update not-main.tf

--- not-main.tf
+++ not-main.tf
@@ -1,19 +1,19 @@
-resource "local_file" "my_trees" {
-    filename = "plants.txt"
-    content = "I'll preserve the nature!"
-}
-
-resource "random_pet" "my-trees" {
-    prefix = "unique/any_name"
-    separator = "."
-    length = "2"
-}
-
+#resource "local_file" "my_trees" {
+#    filename = "plants.txt"
+#    content = "I'll preserve the nature!"
+#}
+#
+#resource "random_pet" "my-trees" {
+#    prefix = "unique/any_name"
+#    separator = "."
+#    length = "2"
+#}
+#
 # resource "local_file" "object-name-01-TF" {
 #     filename = "indoor-plants-${random_pet.name.id}.txt"
 #     content = "Herbivorous are dependent on plants"
 # }
-
+#
 # resource "random_pet" "object-name-02-TF" {
 #     prefix = "unq-id"
 #     separator = "-"
