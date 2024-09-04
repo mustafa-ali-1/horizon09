adad679154976bd140be448297fe6b7b8019ad25
mustafa-ali-1 <mustafa_ali_1@outlook.com>
2024-09-04 18:07:51 +0530

    Update main-03.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "local_file" "my_trees-1" {
+resource "local_file" "my_trees_1" {
     filename = "plants.txt"
     content = "save nature!"
 }
-resource "random_pet" "my-trees_1" {
+resource "random_pet" "my-trees-1" {
     prefix = "unique/any_name"
     separator = "."
     length = "2"
